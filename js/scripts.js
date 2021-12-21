bcb50126fbf515702d3852f779852801eaf31d0d
Samuel Izuagbe Enakhe <izuagbesamuel73@gmail.com>
2021-12-21 23:22:48 +0100

    Finish project ready to symbit

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(){
+   document.getElementById('form1').addEventListener('submit', function() {
+      document.getElementById('modalForm').style.display = "none";
+   })
+})
